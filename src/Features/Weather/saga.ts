118b56d225704e86d2c691b6ab05678063ac9b93
mkiran666 <katapal-k>
2021-06-29 23:11:50 +0530

    saga issues fixed

--- src/Features/Weather/saga.ts
+++ src/Features/Weather/saga.ts
@@ -7,3 +7,6 @@ function* apiErrorReceived(action: PayloadAction<ApiErrorAction>) {
   yield call(toast.error, `Error Received: ${action.payload.error}`);
 }
 
+export default function* watchApiError() {
+  yield takeEvery(WeatherActions.weatherApiErrorReceived.type, apiErrorReceived);
+}
